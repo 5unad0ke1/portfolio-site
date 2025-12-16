2f14355217d862ec196c5a9ab0e100b998d92bd8
Oikawa <yucchi0424@gmail.com>
2025-12-16 21:37:04 +0900

    [update] Move skill animations from skills.js to anime.js

---
+++ js/anime.js
@@ -0,0 +1,26 @@
+const observerEnter = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show'); // 範囲内
+        }
+  });
+},
+{
+    rootMargin: "0px 0px -100px 0px",
+  threshold: 1
+}); // 20%見えたら発火
+
+const observerExit = new IntersectionObserver((entiries) => {
+    entiries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            entry.target.classList.remove('show'); // 範囲外で消える
+        }
+    });
+},
+{
+    threshold: 0.1
+})
+
+const skills = document.querySelectorAll('.animate-on-scroll')
+skills.forEach(skill => observerEnter.observe(skill));
+skills.forEach(skill => observerExit.observe(skill));
