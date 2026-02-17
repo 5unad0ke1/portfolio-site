83c369bafbbdea025af6379c2053111c984a8cfa
Oikawa <yucchi0424@gmail.com>
2026-02-17 22:10:18 +0900

    [update] astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
 export default defineConfig({
+  site: 'https://5unad0ke1.github.io/',
+  base: '/portfolio-site/',
+  
   vite: {
     plugins: [yaml()],
   },
+  output: 'static',
 });
