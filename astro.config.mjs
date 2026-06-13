c52fe1d4f1d59ea963ea0eede5bdef1368ac6ed7
Oikawa <yucchi0424@gmail.com>
2026-06-13 12:26:59 +0900

    [update] configのURLを独自ドメインに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
 export default defineConfig({
-  site: 'https://5unad0ke1.github.io/',
-  base: '/portfolio-site/',
+  site: 'https://portfolio.sunadokei.dev/',
+  base: '/',
   output: 'static',
   
   vite: {
